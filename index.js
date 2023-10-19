77f703b5afdbcff4d742326b75da8006ce181318
Jahirul83 <jahirulislamcse83.com>
2023-10-20 03:00:29 +0600

    data insertion

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,13 +10,62 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// brandDB
+// XdC09yw2oeeqwlxj
 
 
-app.get('/',(req,res)=>{
+
+
+const uri = "mongodb+srv://brandDB:XdC09yw2oeeqwlxj@cluster0.i3hf6sp.mongodb.net/?retryWrites=true&w=majority";
+
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.i3hf6sp.mongodb.net/?retryWrites=true&w=majority`;
+
+// console.log(uri);
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        const productCollection = client.db('productDB').collection('products')
+
+
+        app.post('/products', async (req, res) => {
+
+            const newProduct = req.body;
+            console.log(newProduct);
+            const result = await productCollection.insertOne(newProduct);
+            res.send(result);
+
+        })
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+app.get('/', (req, res) => {
     res.send('Brand Shop server is running');
 })
 
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`Brand shop app listening on port ${port}`);
 })
